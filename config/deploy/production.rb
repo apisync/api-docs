8c87802840a7a6891231177d89badbdc0a328580
Alexandre de Oliveira <kurko@users.noreply.github.com>
2018-11-25 18:54:38 -0200

    fix server host

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,2 @@
-server "veiculo.online", user: ENV.fetch('APISYNC_DEPLOY_USER'), roles: ['web']
+server "apisync.io", user: ENV.fetch('APISYNC_DEPLOY_USER'), roles: ['web']
+set :branch, ENV.fetch('BRANCH', 'master')
